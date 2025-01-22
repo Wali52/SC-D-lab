90199e87a821a07286da39173215c4f186ad3b62
Wali Ahmed <154537007+Wali52@users.noreply.github.com>
2025-01-22 09:46:30 +0500

    Add files via upload

---
+++ HH.java
@@ -0,0 +1,19 @@
+package PH;
+
+public class HH {
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isOdd(int num) {
+        return num % 2 != 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is 2 even? " + isEven(2)); 
+        System.out.println("Is 2 odd? " + isOdd(2));   
+        System.out.println("Is 3 even? " + isEven(3)); 
+        System.out.println("Is 3 odd? " + isOdd(3));   
+    }
+}
